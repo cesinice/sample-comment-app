cef7c5e0ace17101b5ca8a59e0c01b5ab8def429
Antoine Orfila <antoine.orfila@viacesi.fr>
2017-11-30 14:02:29 +0100

    Merge remote-tracking branch 'origin/feature/Post-comment' into feature/Post-comment
    # Conflicts:
    #	components/CommentPost.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-
+  <section class="container">
+    <div>
+      <post-comment></post-comment>
+    </div>
+  </section>
 </template>
 
 <script>
+  import PostComment from '~/components/CommentPost.vue'
 
+  export default {
+    components: {
+      PostComment
+    }
+  }
 </script>
 
 <style>
