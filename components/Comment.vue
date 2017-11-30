f2ed78b488be5dc77030919c8a16a0293c66a3c0
Yarol <yarol.moretti@viacesi.fr>
2017-11-30 12:00:57 +0100

    Created Comment Component

---
+++ components/Comment.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="container">
+    <div class="box">
+      <article class="media">
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>{{ author }}</strong>
+              <br>
+              <textarea class="textarea">{{ comment }}</textarea>
+            </p>
+          </div>
+          <div class="field is-grouped">
+            <p class="control">
+              <a class="button is-link is-focused">
+                Edit
+              </a>
+            </p>
+            <p class="control">
+              <a class="button is-danger is-focused">
+                Delete
+              </a>
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    components: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
