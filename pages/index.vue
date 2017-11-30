cc383272cadd74ab831f6912e66153bfac4b901d
Antoine Orfila <antoine.orfila@viacesi.fr>
2017-11-30 14:06:36 +0100

    Merge 2 features

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <section class="container">
     <div>
       <post-comment></post-comment>
+      <comments></comments>
     </div>
   </section>
 </template>
 
 <script>
   import PostComment from '~/components/CommentPost.vue'
+  import Comments from '~/components/Comments.vue'
 
   export default {
     components: {
-      PostComment
+      PostComment,
+      Comments
     }
   }
 </script>
